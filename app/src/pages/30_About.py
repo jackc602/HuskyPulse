d7077f2e1f07e9ea130b9c448f4ad3a2516982ad
swish05 <sahni.s@northeastern.edu>
2025-04-12 20:04:13 -0400

    changed the about section of HuskyPulse in the About.py file

--- app/src/pages/30_About.py
+++ app/src/pages/30_About.py
@@ -8,10 +8,14 @@ st.write("# About this App")
 
 st.markdown (
     """
-    This is a demo app for CS 3200 Course Project.  
+    Hello! Welcome to the HuskyPulse!! We know how much clubs and events are valued at Northeastern,
+    so we thought it would be a great idea to give you all things related to that right 
+    here on HuskyPulse!
 
-    The goal of this demo is to provide information on the tech stack 
-    being used as well as demo some of the features of the various platforms. 
+    HuskyPulse offers everything you could want from an app - customer reviews, feedback on clubs, 
+    personalized offerings tailored to your interests, and the best part is that it doesn't stop there. 
+    HuskyPulse is the first of it's kind at Northeastern, and we hope we could develop this model to a 
+    larger scale, so it can be of use to all students here at Norheastern. 
 
     Stay tuned for more information and features to come!
     """
