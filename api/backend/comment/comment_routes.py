da8fe8c6784b600839c0f72d5ce5e09ccf40fda5
Jack Carroll <carroll.jac@northeastern.edu>
2025-04-15 12:05:32 -0400

    added functionality for club to see their previous posts, minus seeing rsvps

--- api/backend/comment/comment_routes.py
+++ api/backend/comment/comment_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, make_response
 from backend.db_connection import db
 
 comments = Blueprint('comments', __name__)
@@ -17,3 +17,21 @@ def get_comments():
     cursor.execute(query, (nuid, nuid))
     data = cursor.fetchall()
     return jsonify(data)
+
+# Get comments made on a given post
+@comments.route("/post", methods = ["GET"])
+def get_post_comments():
+    post_id = request.args.get("post_id")
+    query = """
+    SELECT s.first_name AS first, s.last_name AS last, c.text AS content, c.date AS date_posted
+    FROM student s JOIN comment c JOIN post_comment pc
+    ON s.NUID = c.NUID AND c.id = pc.comment_id
+    WHERE pc.post_id = %s
+    """
+    params = (post_id)
+    cursor = db.get_db().cursor()
+    cursor.execute(query, params)
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
