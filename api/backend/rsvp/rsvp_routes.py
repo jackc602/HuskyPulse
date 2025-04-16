ce2b23884c3b69410d13da99a7db75e6705bbe45
swish05 <sahni.s@northeastern.edu>
2025-04-16 10:37:27 -0400

    rsvp route made

--- api/backend/rsvp/rsvp_routes.py
+++ api/backend/rsvp/rsvp_routes.py
@@ -6,3 +6,21 @@ from backend.db_connection import db
 
 rsvp = Blueprint("rsvps", __name__)
 
+@rsvp.route('/rsvp', methods=['GET'])
+def get_products():
+    query = '''
+        SELECT  NUID, 
+                when_rsvped, 
+                name, 
+                start_date, 
+                end_date,
+                location_id
+        FROM event e JOIN student_events se
+        ON se.event_id = e.id
+    '''
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    response = make_response(jsonify(data))
+    response.status_code = 200
+    return response
